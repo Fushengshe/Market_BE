1655d35e7880dcfbed2a31d41d26aa22aebd4ca0
strikergx <1540325125@qq.com>
2017-06-22 21:31:52 +0800

    按需求返回json格式

--- application/admin/model/AuthMod.php
+++ application/admin/model/AuthMod.php
@@ -30,14 +30,14 @@ class AuthMod extends Model
             $userInfo = $this->where('password',sha1(md5($data['password'].$db[0]['salt'])))->find();
             if($userInfo){
                 $token=AuthMod::setToken($db[0]['id']);
-                return json([ 'token' => $token, 'id' => $db[0]['id'] , 'mobile' => $data['mobile'] ]);
+                return json(['code'=>0,'data'=>['user'=>['userId'=>$db[0]['id'],'mobile'=>$data['mobile']]],'token'=>$token]);
             }
             else{
-                return json(['status'=>0,'msg'=>'密码错误']);
+                return json(['code'=>0,'msg'=>'密码错误']);
             }
         }
         else{
-            return json(['status'=>0,'msg'=>'用户不存在']);
+            return json(['code'=>0,'msg'=>'用户不存在']);
         }
     }
     public static function setToken($id)
@@ -58,19 +58,25 @@ class AuthMod extends Model
         $res = Db::table('user')
             ->where('token',$token)
             ->select();
-        if (time() - $res[0]['token_exp'] > 0)
-        {
-            return 90003;  //token长时间未使用而过期，需重新登陆
+        if ($res[0][$token]==$token){
+            if (time() - $res[0]['token_exp'] > 0)
+            {
+                return 90003;  //token长时间未使用而过期，需重新登陆
+            }
+            else
+            {
+                Db::table('user')
+                    ->where('token',$token)
+                    ->update(['token_exp'=>time()+86400]);
+                return 90001;  //token验证成功，time_out刷新成功，可以获取接口信息
+            }
         }
         else
         {
-            Db::table('user')
-                ->where('token',$token)
-                ->update(['token_exp'=>time()+86400]);
-            return 90001;  //token验证成功，time_out刷新成功，可以获取接口信息
+            return 90002;  //token错误验证失败
         }
 
-        return 90002;  //token错误验证失败
+
     }
     public function signup($username,$password,$mobile,$confirm){
         $admin  = \think\Db::name('user')->where('username','=',$username)->find();
@@ -90,6 +96,5 @@ class AuthMod extends Model
             return 1;
         }
     }
-
 
 }
